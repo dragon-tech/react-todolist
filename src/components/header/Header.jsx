618d3000da6d5e47a45465fceee8d6c40cea6a11
dragon-tech <66893576+dragon-tech@users.noreply.github.com>
2021-08-17 00:27:11 +0700

    header component css modules

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Button from "./button/Button";
+import Button from "../button/Button";
+import styles from "./header.module.css";
 const Header = ({ showAddToggle,showAdd,clearTodos }) => {
     return (
-        <section className="header">  
+        <section className={ styles.header }>  
           <Button text={ showAdd ? 'FINISH' : 'ADD' } onClick={ showAddToggle } />
-          <h1 className="header-title">Todo List</h1>
+          <h1 className={ styles.headerTitle }>Todo List</h1>
           <Button text="Clear" onClick={ clearTodos } color="red" align="right" />
         </section>
     );
